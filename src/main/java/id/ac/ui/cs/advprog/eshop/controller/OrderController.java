02e8b3d105bbc4b8fb92bfb16666d8a63c681620
josiahsimorangkir <josiahsimorangkir@gmail.com>
2026-03-10 21:39:36 +0700

    fix some issues

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.OrderService;
 import id.ac.ui.cs.advprog.eshop.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/order")
@@ -25,6 +28,23 @@ public class OrderController {
     @GetMapping("/create")
     public String createOrderPage(Model model) {
         return "orderCreate";
+    }
+
+    @PostMapping("/create")
+    public String createOrderPost(@RequestParam String author, @RequestParam(required = false, defaultValue = "Produk Default") String productName) {
+
+        Product dummyProduct = new Product();
+        dummyProduct.setProductId(UUID.randomUUID().toString());
+        dummyProduct.setProductName(productName);
+        dummyProduct.setProductQuantity(1);
+
+        List<Product> dummyProductList = new ArrayList<>();
+        dummyProductList.add(dummyProduct);
+
+        Order newOrder = new Order(UUID.randomUUID().toString(), dummyProductList, System.currentTimeMillis(), author);
+
+        orderService.createOrder(newOrder);
+        return "redirect:/order/history";
     }
 
     @GetMapping("/history")
