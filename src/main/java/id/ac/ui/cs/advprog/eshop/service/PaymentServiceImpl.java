6fa92a6fc8e47f8b85cdad8130f7d38adbfebb38
josiahsimorangkir <josiahsimorangkir@gmail.com>
2026-03-10 17:36:34 +0700

    [GREEN] Implement PaymentServiceImpl logic

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,68 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        String paymentId = UUID.randomUUID().toString();
+        String status = "REJECTED";
+
+        if ("VOUCHER".equals(method)) {
+            String voucherCode = paymentData.get("voucherCode");
+            if (voucherCode != null && voucherCode.length() == 16 && voucherCode.startsWith("ESHOP")) {
+                int numCount = 0;
+                for (char c : voucherCode.toCharArray()) {
+                    if (Character.isDigit(c)) numCount++;
+                }
+                if (numCount == 8) {
+                    status = "SUCCESS";
+                }
+            }
+        } else if ("CASH_ON_DELIVERY".equals(method)) {
+            String address = paymentData.get("address");
+            String deliveryFee = paymentData.get("deliveryFee");
+            if (address != null && !address.trim().isEmpty() && deliveryFee != null && !deliveryFee.trim().isEmpty()) {
+                status = "SUCCESS";
+            }
+        }
+
+        Payment payment = new Payment(paymentId, method, status, paymentData, order);
+        paymentRepository.save(payment);
+        return payment;
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status) {
+        payment.setStatus(status);
+        if ("SUCCESS".equals(status)) {
+            payment.getOrder().setStatus("SUCCESS");
+        } else if ("REJECTED".equals(status)) {
+            payment.getOrder().setStatus("FAILED");
+        }
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public Payment getPayment(String paymentId) {
+        return paymentRepository.findById(paymentId);
+    }
+
+    @Override
+    public List<Payment> getAllPayments() {
+        return paymentRepository.getAllPayments();
+    }
+}
